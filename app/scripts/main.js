c088aed15f76178442511b42059253a693315b6c
1silversnow1 <vladislavsnow@gmail.com>
2017-07-20 20:34:18 +0300

    Filter select updated

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -29,4 +29,73 @@ $(document).ready(function() {
             $('#ui-datepicker-div').addClass(this.id);
         }
     });
+
+    $( "#header_out_date_picker" ).datepicker({
+        beforeShow: function(input, inst) {
+            $('#header_out_date_picker').removeClass(function() {
+                return $('input').get(0).id;
+            });
+            $('#ui-datepicker-div').addClass(this.id);
+        }
+    });
+
+    window.inputNumber = function(el) {
+
+        let min = el.attr('min') || false;
+        let max = el.attr('max') || false;
+
+        let els = {};
+
+        els.dec = el.prev();
+        els.inc = el.next();
+
+        el.each(function() {
+            init($(this));
+        });
+
+        function init(el) {
+
+            els.dec.on('click', decrement);
+            els.inc.on('click', increment);
+
+            function decrement($event) {
+                $event.stopPropagation();
+                let value = el[0].value;
+                value--;
+                if(!min || value >= min) {
+                    el[0].value = value;
+                }
+            }
+
+            function increment($event) {
+                $event.stopPropagation();
+                let value = el[0].value;
+                value++;
+                if(!max || value <= max) {
+                    el[0].value = value++;
+                }
+            }
+        }
+    };
+    inputNumber($('.input-number'));
+
+
+    let $html = $('html');
+    $html.on('click.ui.dropdown', '.js-dropdown', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('is-open');
+    });
+    $html.on('click.ui.dropdown', '.js-dropdown [data-dropdown-value]', function(e) {
+        e.preventDefault();
+        let $item = $(this);
+        let $dropdown = $item.parents('.js-dropdown');
+        $dropdown.find('.js-dropdown__input').val($item.data('dropdown-value'));
+        $dropdown.find('.js-dropdown__current').text($item.text());
+    });
+    $html.on('click.ui.dropdown', function(e) {
+        let $target = $(e.target);
+        if (!$target.parents().hasClass('js-dropdown')) {
+            $('.js-dropdown').removeClass('is-open');
+        }
+    });
 });
